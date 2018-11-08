42aa5987dff0cf85371154b3fe5ed8003ee895ed
磯崎裕太 <yutaisozaki@isozakiyuutanoMacBook-puro.local>
2018-11-08 11:28:56 +0900

    notification

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile << 'form/signup.css'
 Rails.application.config.assets.precompile << 'nomiya/index'
 Rails.application.config.assets.precompile << 'nomiya/show'
+Rails.application.config.assets.precompile << 'users/show'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
